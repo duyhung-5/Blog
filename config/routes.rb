e987edf9a585c89245630fbeb2501e1ba881167a
duyhung-5 <phamduyh77@gmail.com>
2025-01-09 15:28:13 +0700

    init project

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
+  resource :session
+  resource :unsubscribe, only: [ :show ]
+  resources :passwords, param: :token
   resources :products
 
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
